79225c2d9795c7c56ccf053c8897517b6849381e
Greg Brilev <gbrilev@gmail.com>
2019-07-09 09:54:32 +0300

    fixed null.toLowerCase() issue at uuidV4Opt

--- test/specs/filtra.uuidv4.spec.js
+++ test/specs/filtra.uuidv4.spec.js
@@ -7,15 +7,23 @@ const genUuidv4 = require('uuid/v4')
 describe('.uuidV4', () => {
   describe('.uuidV4(key, uuidV4())', () => {
     it('writes generated UUIDs', () => {
-      for (let i = 0; i < 10; i++) {
+      for (let i = 0; i < 30; i++) {
         const uuid = genUuidv4()
         let generated = Filtra({}).uuidV4('x', genUuidv4()).filter().x
         should.equal(Filtra({x: generated}).uuidV4('x').filter().x, generated)
-        generated = Filtra({}).uuidV4('x', genUuidv4()).filter().x
-        should.equal(Filtra({x: generated}).uuidV4('x').filter().x, generated)
-        generated = Filtra({}).uuidV4('x', genUuidv4()).filter().x
-        should.equal(Filtra({x: generated}).uuidV4('x').filter().x, generated)
       }
+    })
+  })
+  describe('.uuidV4Opt(key)', () => {
+    it('handles empty value', () => {
+        const nullval = Filtra({}).uuidV4Opt('x').filter().x
+        should.equal(nullval, null)
+    })
+  })
+  describe('.uuidV4Opt(key)', () => {
+    it('handles not empty value', () => {
+      const uuid = genUuidv4()
+      should.equal(Filtra({x: uuid}).uuidV4('x').filter().x, uuid)
     })
   })
 })
